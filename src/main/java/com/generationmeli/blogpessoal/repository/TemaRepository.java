4f4e3cdf3f1ad5581af63e6fdd6741342117c34f
“Silvio <silvio.dossantos@mercadolivre.com>
2022-11-25 19:22:33 -0300

    Foi criado a ta_tema conforme o documentação, e  inserido todos os métodos.

---
+++ src/main/java/com/generationmeli/blogpessoal/repository/TemaRepository.java
@@ -0,0 +1,15 @@
+package com.generationmeli.blogpessoal.repository;
+
+import com.generationmeli.blogpessoal.model.Tema;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TemaRepository extends JpaRepository<Tema, Long> {
+
+    public List<Tema> findAllByDescricaoContainingIgnoreCase(@Param("descricao") String descricao);
+
+}
