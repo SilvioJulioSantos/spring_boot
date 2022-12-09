12c8b2b0e2086e6d2fe0fe484bb4c7aa3341fb09
“Silvio <silvio.dossantos@mercadolivre.com>
2022-12-08 21:34:11 -0300

    Atividade teste com o JUNIT- back-end projeto do Blog pessoal finalizada.

--- src/main/java/com/generationmeli/blogpessoal/repository/UsuarioRepository.java
+++ src/main/java/com/generationmeli/blogpessoal/repository/UsuarioRepository.java
@@ -4,12 +4,17 @@ package com.generationmeli.blogpessoal.repository;
 
 import com.generationmeli.blogpessoal.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     public Optional<Usuario> findByUsuario(String usuario);
+
+    public List<Usuario> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 
 }
